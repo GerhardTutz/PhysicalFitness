5893e04f2e1712436e519bbc2d00a3c24110b392
GerhardTutz <70742998+GerhardTutz@users.noreply.github.com>
2024-09-15 18:19:49 +0200

    Add files via upload

---
+++ Crime.R
@@ -0,0 +1,13 @@
+
+
+##### Crime
+
+
+
+#getwd()
+#setwd("C:\\Users\\Gerhard Tutz\\LRZ Sync+Share\\ABookRasch\\R\\Counts")
+
+
+
+policecrime24=read.csv("tablep3edited.csv", header=T, row.names =1)
+
